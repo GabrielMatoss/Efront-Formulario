d5dc48bb918614665fe8be9659c7e20ad1297528
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-07-17 11:22:32 -0300

    feat(form): adding creation of form component, and reset of form fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { useState } from "react";
+import { Form } from "./assets/components/Form";
 import eFrontLogo from "./assets/eFront.svg";
 
-interface appFormProps {
-  name: string;
-  email: string;
-}
 //pelo que eu entendi, vamos ter que criar de alguma forma um sistema que armazene as contas criadas,
 // e as verifique e acesse a rota se ja tiver cadastrado ou nao.
 export function App() {
-  const [formState, setFormState] = useState<appFormProps>({
-    name: "",
-    email: "",
-  });
-
-  function handleInput(event: any, key: any) {
-    setFormState((state) => {
-      return { ...state, [key]: event.target.value };
-    });
-  }
-
-  function formEventInput(event: any) {
-    event.preventDefault();
-    console.log(formState);
-  }
   return (
     <div className="max-w-7xl mx-auto pb-4">
       <header className="pt-8 px-2 flex items-center justify-between">
@@ -54,52 +35,7 @@ export function App() {
             Preencha os campos para receber as notícias sobre programação
           </p>
 
-          <form className="mt-8 space-y-7" onSubmit={formEventInput}>
-            <div className="flex flex-col">
-              <label
-                htmlFor="name"
-                className="font-Inter font-medium text-sm text-gray-primary"
-              >
-                Nome
-              </label>
-              <input
-                value={formState.name}
-                onChange={(event) => handleInput(event, "name")}
-                type="text"
-                name="name"
-                id="name"
-                className="bg-dark-primary h-8 rounded border border-dark-secondary
-                pl-2 text-gray-primary focus:outline-none focus:border-green-primary"
-                required
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <label
-                htmlFor="email"
-                className="font-Inter font-medium text-sm text-gray-primary"
-              >
-                E-mail
-              </label>
-              <input
-                value={formState.email}
-                onChange={(event) => handleInput(event, "email")}
-                name="email"
-                type="email"
-                id="email"
-                className="bg-dark-primary h-8 rounded border border-dark-secondary
-                pl-2 text-gray-primary"
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="flex items-center justify-center bg-green-primary w-full py-1 font-Inter font-semibold 
-              text-lg text-dark-primary rounded disabled:cursor-not-allowed disabled:opacity-60"
-            >
-              Enviar
-            </button>
-          </form>
+          <Form />
         </section>
       </main>
     </div>
---
+++ src/assets/components/Form.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+interface appFormProps {
+  name: string;
+  email: string;
+}
+export function Form() {
+  const [formState, setFormState] = useState<appFormProps>({
+    name: "",
+    email: "",
+  });
+
+  function handleInput(event: any, key: any) {
+    setFormState((state) => {
+      return { ...state, [key]: event.target.value };
+    });
+  }
+
+  function formEventInput(event: any) {
+    event.preventDefault();
+    console.log(formState);
+    setFormState({
+      name: "",
+      email: "",
+    });
+  }
+  return (
+    <form className="mt-8 space-y-7" onSubmit={formEventInput}>
+      <div className="flex flex-col">
+        <label
+          htmlFor="name"
+          className="font-Inter font-medium text-sm text-gray-primary"
+        >
+          Nome
+        </label>
+        <input
+          value={formState.name}
+          onChange={(event) => handleInput(event, "name")}
+          type="text"
+          name="name"
+          id="name"
+          className="bg-dark-primary h-8 rounded border border-dark-secondary
+                pl-2 text-gray-primary focus:outline-none focus:border-green-primary"
+          required
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label
+          htmlFor="email"
+          className="font-Inter font-medium text-sm text-gray-primary"
+        >
+          E-mail
+        </label>
+        <input
+          value={formState.email}
+          onChange={(event) => handleInput(event, "email")}
+          name="email"
+          type="email"
+          id="email"
+          className="bg-dark-primary h-8 rounded border border-dark-secondary
+                pl-2 text-gray-primary"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="flex items-center justify-center bg-green-primary w-full py-1 font-Inter font-semibold 
+              text-lg text-dark-primary rounded disabled:cursor-not-allowed disabled:opacity-60"
+      >
+        Enviar
+      </button>
+    </form>
+  );
+}
