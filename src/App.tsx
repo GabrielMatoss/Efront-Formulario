09b512b53cb961d69ec907588f1fe57344a424cc
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-07-20 10:54:02 -0300

    style(app): adding hover and transitions to buttons and inputs

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ export function App() {
 
         <button
           type="button"
-          className="font-sans text-sm font-bold py-3 px-4 border border-green-primary rounded-md uppercase
-          text-white-primary"
+          className="font-sans text-sm font-bold py-3 px-4 border-2 border-green-primary 
+          hover:border-opacity-70 transition-colors rounded-md uppercase text-white-primary"
         >
           comprar ebook
         </button>
       </header>
 
-      <main className="max-w-md mx-auto mt-[70px]">
+      <main className="max-w-md mx-auto mt-14">
         <section className="text-center flex flex-col items-center justify-between gap-5">
           <h1 className="font-Sora text-3xl leading-normal font-semibold text-white-primary">
             Seja bem-vindo a
@@ -28,7 +28,7 @@ export function App() {
           </p>
         </section>
 
-        <section className="mt-8 px-7">
+        <section className="mt-6 px-7">
           <p className="text-gray-primary font-Inter font-medium text-base leading-normal text-center">
             Preencha os campos para receber as notícias sobre programação
           </p>
